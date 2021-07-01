c33ca5d2f2be6363483d5cca92aea6ebe02dd2ee
Miguel Angel Romero <miguelromero717@gmail.com>
2021-07-01 14:40:40 -0500

    Add functions tests

--- 03-counter-app/src/base/05-funciones.js
+++ 03-counter-app/src/base/05-funciones.js
@@ -13,28 +13,28 @@ const saludar4 = () => `Hola Mundo`;
 
 // console.log( saludar('Goku') )
 
-console.log( saludar2('Vegeta') );
-console.log( saludar3('Goku') );
-console.log( saludar4() );
+// console.log( saludar2('Vegeta') );
+// console.log( saludar3('Goku') );
+// console.log( saludar4() );
 
 
-const getUser = () => ({
+export const getUser = () => ({
         uid: 'ABC123',
         username: 'El_Papi1502'
 });
 
 
 const user = getUser();
-console.log(user);
+// console.log(user);
 
 // Tarea
-const getUsuarioActivo = ( nombre ) =>({
+export const getUsuarioActivo = ( nombre ) =>({
     uid: 'ABC567',
     username: nombre
 })
 
 const usuarioActivo = getUsuarioActivo('Fernando');
-console.log( usuarioActivo );
+// console.log( usuarioActivo );
 
 
 
---
+++ 03-counter-app/src/tests/05-funciones.test.js
@@ -0,0 +1,26 @@
+import { getUser, getUsuarioActivo } from "../base/05-funciones";
+
+describe('05-funciones Tests', () => {
+    test('getUser should returns an object', () => {
+        const userTest = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        };
+        const user = getUser();
+
+        expect(user).toEqual(userTest);
+    })
+
+    test('getUserActivo should returns an object', () => {
+        const name = 'Miguel';
+        const userActiveTest = {
+            uid: 'ABC567',
+            username: name
+        };
+        const userActive = getUsuarioActivo(name);
+
+        expect(userActive).toEqual(userActiveTest);
+    })
+
+
+})
